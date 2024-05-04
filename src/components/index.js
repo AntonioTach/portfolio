c03788bb472e618bd4b7b4d55a0459212527a9de
AntonioTach <antach.vihe@gmail.com>
2024-05-03 22:41:22 -0600

    FEAT: More components worked

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { BallCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
@@ -18,9 +18,7 @@ export {
   Works,
   Feedbacks,
   Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
+  BallCanvas,
   StarsCanvas,
   Background,
 }
