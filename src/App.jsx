62b1e174373a5ba85ea904adac02815facf11ff2
AntonioTach <antach.vihe@gmail.com>
2024-05-03 23:04:07 -0600

    FIX - Revert for files and Contact & Feedback

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,12 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <Feedbacks />
         <div className="relative z-0">
           <About />
           <Experience />
           <Tech />
           <Works />
-
+          <Feedbacks />
           <Contact />
           <StarsCanvas />
         </div>
